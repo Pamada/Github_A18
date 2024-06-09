2e68f338befac0e995920126ccd200fedd2fd7d0
dana3 <dana3pal@gmail.com>
2024-06-09 19:47:52 -0400

    Github_A18  first commit

---
+++ src/test/java/com/jdk/explore/java/github_a18/GithubA18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdk.explore.java.github_a18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubA18ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
